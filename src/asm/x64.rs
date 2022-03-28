b374b1e6c615654bbb02882484284313a62742d2
Roddy MacCrimmon <r.maccrimm@gmail.com>
2022-03-27 23:18:35 -0600

    add test for push/pop operations

--- src/asm/x64.rs
+++ src/asm/x64.rs
@@ -230,6 +230,7 @@ mod tests {
             assert_eq!(code, vec![0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57]);
         }
         #[test]
+        #[rustfmt::skip]
         fn test_push_reg64_extended() {
             let mut code: Vec<u8> = Vec::new();
             push_reg64(&mut code, RegX64::R8);
@@ -240,13 +241,148 @@ mod tests {
             push_reg64(&mut code, RegX64::R13);
             push_reg64(&mut code, RegX64::R14);
             push_reg64(&mut code, RegX64::R15);
-            assert_eq!(
-                code,
-                vec![
-                    0x41, 0x50, 0x41, 0x51, 0x41, 0x52, 0x41, 0x53, 0x41, 0x54, 0x41, 0x55, 0x41,
-                    0x56, 0x41, 0x57,
-                ]
-            );
+            assert_eq!(code, vec![
+                0x41, 0x50, 
+                0x41, 0x51, 
+                0x41, 0x52,
+                0x41, 0x53, 
+                0x41, 0x54, 
+                0x41, 0x55, 
+                0x41, 0x56, 
+                0x41, 0x57,
+            ]);
+        }
+        #[test]
+        #[rustfmt::skip]
+        fn test_push_ptr64_base() {
+            let mut code: Vec<u8> = Vec::new();
+            push_ptr64(&mut code, RegX64::RAX);
+            push_ptr64(&mut code, RegX64::RCX);
+            push_ptr64(&mut code, RegX64::RDX);
+            push_ptr64(&mut code, RegX64::RBX);
+            push_ptr64(&mut code, RegX64::RSP);
+            push_ptr64(&mut code, RegX64::RBP);
+            push_ptr64(&mut code, RegX64::RSI);
+            push_ptr64(&mut code, RegX64::RDI);
+            assert_eq!(code, vec![
+                0xff, 0x30, 
+                0xff, 0x31, 
+                0xff, 0x32, 
+                0xff, 0x33, 
+                0xff, 0x34, 0x24, 
+                0xff, 0x75, 0x00, 
+                0xff, 0x36, 
+                0xff, 0x37,
+            ]);
+        }
+        #[test]
+        #[rustfmt::skip]
+        fn test_push_ptr64_extended() {
+            let mut code: Vec<u8> = Vec::new();
+            push_ptr64(&mut code, RegX64::R8);
+            push_ptr64(&mut code, RegX64::R9);
+            push_ptr64(&mut code, RegX64::R10);
+            push_ptr64(&mut code, RegX64::R11);
+            push_ptr64(&mut code, RegX64::R12);
+            push_ptr64(&mut code, RegX64::R13);
+            push_ptr64(&mut code, RegX64::R14);
+            push_ptr64(&mut code, RegX64::R15);
+            assert_eq!(code, vec![
+                0x41, 0xff, 0x30, 
+                0x41, 0xff, 0x31, 
+                0x41, 0xff, 0x32, 
+                0x41, 0xff, 0x33, 
+                0x41, 0xff, 0x34, 0x24, 
+                0x41, 0xff, 0x75, 0x00, 
+                0x41, 0xff, 0x36, 
+                0x41, 0xff, 0x37,
+            ]);
+        }
+    }
+    #[cfg(test)]
+    mod pop {
+        use super::*;
+        #[test]
+        fn test_pop_reg64_base() {
+            let mut code: Vec<u8> = Vec::new();
+            pop_reg64(&mut code, RegX64::RAX);
+            pop_reg64(&mut code, RegX64::RCX);
+            pop_reg64(&mut code, RegX64::RDX);
+            pop_reg64(&mut code, RegX64::RBX);
+            pop_reg64(&mut code, RegX64::RSP);
+            pop_reg64(&mut code, RegX64::RBP);
+            pop_reg64(&mut code, RegX64::RSI);
+            pop_reg64(&mut code, RegX64::RDI);
+            assert_eq!(code, vec![0x58, 0x59, 0x5a, 0x5b, 0x5c, 0x5d, 0x5e, 0x5f]);
+        }
+        #[test]
+        #[rustfmt::skip]
+        fn test_pop_reg64_extended() {
+            let mut code: Vec<u8> = Vec::new();
+            pop_reg64(&mut code, RegX64::R8);
+            pop_reg64(&mut code, RegX64::R9);
+            pop_reg64(&mut code, RegX64::R10);
+            pop_reg64(&mut code, RegX64::R11);
+            pop_reg64(&mut code, RegX64::R12);
+            pop_reg64(&mut code, RegX64::R13);
+            pop_reg64(&mut code, RegX64::R14);
+            pop_reg64(&mut code, RegX64::R15);
+            assert_eq!(code, vec![
+                0x41, 0x58, 
+                0x41, 0x59, 
+                0x41, 0x5a,
+                0x41, 0x5b, 
+                0x41, 0x5c, 
+                0x41, 0x5d, 
+                0x41, 0x5e, 
+                0x41, 0x5f,
+            ]);
+        }
+        #[test]
+        #[rustfmt::skip]
+        fn test_pop_ptr64_base() {
+            let mut code: Vec<u8> = Vec::new();
+            pop_ptr64(&mut code, RegX64::RAX);
+            pop_ptr64(&mut code, RegX64::RCX);
+            pop_ptr64(&mut code, RegX64::RDX);
+            pop_ptr64(&mut code, RegX64::RBX);
+            pop_ptr64(&mut code, RegX64::RSP);
+            pop_ptr64(&mut code, RegX64::RBP);
+            pop_ptr64(&mut code, RegX64::RSI);
+            pop_ptr64(&mut code, RegX64::RDI);
+            assert_eq!(code, vec![
+                0x8f, 0x00, 
+                0x8f, 0x01, 
+                0x8f, 0x02, 
+                0x8f, 0x03, 
+                0x8f, 0x04, 0x24, 
+                0x8f, 0x45, 0x00, 
+                0x8f, 0x06, 
+                0x8f, 0x07,
+            ]);
+        }
+        #[test]
+        #[rustfmt::skip]
+        fn test_pop_ptr64_extended() {
+            let mut code: Vec<u8> = Vec::new();
+            pop_ptr64(&mut code, RegX64::R8);
+            pop_ptr64(&mut code, RegX64::R9);
+            pop_ptr64(&mut code, RegX64::R10);
+            pop_ptr64(&mut code, RegX64::R11);
+            pop_ptr64(&mut code, RegX64::R12);
+            pop_ptr64(&mut code, RegX64::R13);
+            pop_ptr64(&mut code, RegX64::R14);
+            pop_ptr64(&mut code, RegX64::R15);
+            assert_eq!(code, vec![
+                0x41, 0x8f, 0x00, 
+                0x41, 0x8f, 0x01, 
+                0x41, 0x8f, 0x02, 
+                0x41, 0x8f, 0x03, 
+                0x41, 0x8f, 0x04, 0x24, 
+                0x41, 0x8f, 0x45, 0x00, 
+                0x41, 0x8f, 0x06, 
+                0x41, 0x8f, 0x07,
+            ]);
         }
     }
 }
