d59cd047057fec299b658b5e60d3b9bf73d3aebc
Roddy MacCrimmon <r.maccrimm@gmail.com>
2022-03-31 19:22:42 -0600

    add default reg allocation

--- src/asm/alloc.rs
+++ src/asm/alloc.rs
@@ -12,9 +12,48 @@ pub enum MappedReg {
 
 pub struct RegAllocation {
     pub mapping: Vec<MappedReg>,
+    pub num_spilled: u8,
 }
 
 impl RegAllocation {
+    pub fn default() -> RegAllocation {
+        RegAllocation {
+            mapping: vec![
+                MappedReg::Phys(RegX64::RAX),
+                MappedReg::Phys(RegX64::RBX),
+                MappedReg::Phys(RegX64::RSI),
+                MappedReg::Phys(RegX64::RDI),
+                MappedReg::Phys(RegX64::R8),
+                MappedReg::Phys(RegX64::R9),
+                MappedReg::Phys(RegX64::R10),
+                MappedReg::Phys(RegX64::R11),
+                MappedReg::Phys(RegX64::R12),
+                MappedReg::Phys(RegX64::R13),
+                MappedReg::Phys(RegX64::R14),
+                MappedReg::Phys(RegX64::R15),
+                MappedReg::Spill(0),
+                MappedReg::Spill(1),
+                MappedReg::Spill(2),
+                MappedReg::Spill(3),
+                MappedReg::Spill(4),
+                MappedReg::Spill(5),
+                MappedReg::Spill(6),
+                MappedReg::Spill(7),
+                MappedReg::Spill(8),
+                MappedReg::Spill(9),
+                MappedReg::Spill(10),
+                MappedReg::Spill(11),
+                MappedReg::Spill(12),
+                MappedReg::Spill(13),
+                MappedReg::Spill(14),
+                MappedReg::Spill(15),
+                MappedReg::Spill(16),
+                MappedReg::Spill(17),
+            ],
+            num_spilled: 18,
+        }
+    }
+
     // Just uses a naive allocation right now, with registers mapped to physical registers in the
     // order they appear
     pub fn new(instructions: Vec<Instr>) -> RegAllocation {
@@ -33,7 +72,7 @@ impl RegAllocation {
         // Currently RBP and RSP are reserved (should have a better way of doing this)
         let available_pregs = [
             RegX64::RAX,
-            RegX64::RCX,
+            RegX64::RCX, // TODO reserve RDX and RCX
             RegX64::RDX,
             RegX64::RBX,
             RegX64::RSI,
@@ -49,23 +88,34 @@ impl RegAllocation {
         ];
         let mut mapping = vec![MappedReg::Unmapped; 30];
         let mut preg_ind = 0;
+        let mut num_spilled = 0;
         for (i, vreg) in vregs.into_iter().enumerate() {
-            mapping[vreg as usize] = match preg_ind {
-                p if p < available_pregs.len() => MappedReg::Phys(available_pregs[preg_ind]),
-                _ => MappedReg::Spill((i - available_pregs.len()) as u8),
+            let m = if preg_ind < available_pregs.len() {
+                MappedReg::Phys(available_pregs[preg_ind])
+            } else {
+                num_spilled += 1;
+                MappedReg::Spill((i - available_pregs.len()) as u8)
             };
+            mapping[vreg as usize] = m;
             preg_ind += 1;
         }
-        RegAllocation { mapping }
+        RegAllocation {
+            mapping,
+            num_spilled,
+        }
     }
 
     pub fn swap(&mut self, a: VReg, b: VReg) {
         let temp = self.mapping[a as usize];
-        *self.get(a) = *self.get(b);
-        *self.get(b) = temp;
+        *self.get_mut(a) = self.get(b);
+        *self.get_mut(b) = temp;
     }
 
-    fn get(&mut self, reg: VReg) -> &mut MappedReg {
+    pub fn get(&mut self, reg: VReg) -> MappedReg {
+        self.mapping[reg as usize]
+    }
+
+    pub fn get_mut(&mut self, reg: VReg) -> &mut MappedReg {
         &mut self.mapping[reg as usize]
     }
 }
