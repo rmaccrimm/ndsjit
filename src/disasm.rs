53d82d9a04ed486f385f0dad9ebda3fa47af115f
Roddy MacCrimmon <r.maccrimm@gmail.com>
2022-04-09 11:19:30 -0600

    add functions for getting bit range

--- src/disasm.rs
+++ src/disasm.rs
@@ -1,13 +1,21 @@
-use super::ir::Opcode;
+use super::ir::{Opcode, Opcode::*};
 use std::ops::Range;
 
-pub fn bits(byte: u8, r: Range<usize>) -> u8 {
-    assert!(r.start < r.end);
-    (byte >> r.start) & ((1 << (r.end - r.start + 1)) - 1)
+fn le_to_word(bytes: &[u8]) -> u32 {
+    assert!(bytes.len() <= 4);
+    bytes
+        .iter()
+        .enumerate()
+        .fold(0, |acc, (i, &b)| acc | ((b as u32) << (8 * i)))
 }
 
-pub fn bit(byte: u8, b: usize) -> u8 {
-    (byte >> b) & 1
+fn bits(bytes: &[u8], r: Range<usize>) -> u32 {
+    assert!(r.start < r.end);
+    (le_to_word(bytes) >> r.start) & ((1 << (r.end - r.start + 1)) - 1)
+}
+
+fn bit(bytes: &[u8], b: usize) -> u32 {
+    (le_to_word(bytes) >> b) & 1
 }
 
 pub fn try_disasm_arm(instr: &[u8; 4]) -> Option<Opcode> {
@@ -15,18 +23,36 @@ pub fn try_disasm_arm(instr: &[u8; 4]) -> Option<Opcode> {
 }
 
 pub fn try_disasm_thumb(instr: &[u8; 2]) -> Option<Opcode> {
-    None
+    match (bits(instr, 12..15), bit(instr, 9)) {
+        (0b0101, 0) => Some(disasm_ldr_str_thumb(instr)),
+        _ => None,
+    }
 }
+
+fn disasm_ldr_str_arm(instr: &[u8; 4]) -> Opcode {}
+
+fn disasm_ldr_str_thumb(instr: &[u8; 2]) -> Opcode {}
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
+    fn test_to_word() {
+        assert_eq!(le_to_word(&[0xe3, 0xf1, 0x18, 0x77]), 0x7718f1e3);
+        assert_eq!(le_to_word(&[0x91, 0xab, 0x01]), 0x0001ab91);
+        assert_eq!(le_to_word(&[]), 0);
+    }
+
+    #[test]
     fn test_bits() {
-        assert_eq!(bits(0b11101010, 0..3), 0b1010);
-        assert_eq!(bits(0b01000110, 4..7), 0b0100);
-        assert_eq!(bits(0b11101011, 5..6), 0b11);
-        assert_eq!(bit(0b11101011, 7), 1);
-        assert_eq!(bit(0b11101011, 2), 0);
+        assert_eq!(
+            bits(&[0b00000000, 0b11111110, 0b00110010, 0b01110111], 3..27),
+            0b0111001100101111111000000
+        );
+        assert_eq!(
+            bits(&[0b10001100, 0b01000110, 0b10100000, 0b10110001], 19..24),
+            0b110100
+        );
+        assert_eq!(bits(&[0b10111100], 5..12), 0b00000101);
     }
 }
