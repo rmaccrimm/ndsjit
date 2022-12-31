a2b3d80aaa588c056d8f9af5b7f7510d6d0220ab
Roddy MacCrimmon <r.maccrimm@gmail.com>
2022-12-31 11:34:55 -0700

    add function for matching bit patterns

--- src/disasm/bits.rs
+++ src/disasm/bits.rs
@@ -15,3 +15,54 @@ pub fn bit(word: u32, b: usize) -> u32 {
 fn word_align(addr: u32) -> u32 {
     addr & !(0b11)
 }
+
+fn bit_match(x: u32, pattern: &str) -> bool {
+    let mask =
+        u32::from_str_radix(pattern.replace("0", "1").replace("x", "0").as_str(), 2).unwrap();
+    let masked = x & mask;
+    let comp = u32::from_str_radix(pattern.replace("x", "0").as_str(), 2).unwrap();
+    masked == comp
+}
+
+#[cfg(test)]
+mod tests {
+    use super::bit_match;
+
+    #[test]
+    fn test_bit_match() {
+        assert!(bit_match(0b11001, "11xx1"));
+        assert!(bit_match(0b11011, "11xx1"));
+        assert!(bit_match(0b11101, "11xx1"));
+        assert!(bit_match(0b11111, "11xx1"));
+
+        assert!(!bit_match(0b01001, "11xx1"));
+        assert!(!bit_match(0b10011, "11xx1"));
+        assert!(!bit_match(0b11100, "11xx1"));
+        assert!(!bit_match(0b00110, "11xx1"));
+
+        assert!(bit_match(0b00110000100, "0110xx010x"));
+        assert!(bit_match(0b00110000101, "0110xx010x"));
+        assert!(bit_match(0b00110010100, "0110xx010x"));
+        assert!(bit_match(0b00110010101, "0110xx010x"));
+
+        assert!(bit_match(0b00110100100, "0110xx010x"));
+        assert!(bit_match(0b00110100101, "0110xx010x"));
+        assert!(bit_match(0b00110110100, "0110xx010x"));
+        assert!(bit_match(0b00110110101, "0110xx010x"));
+
+        assert!(bit_match(0b10110000100, "0110xx010x"));
+        assert!(bit_match(0b10110000101, "0110xx010x"));
+        assert!(bit_match(0b10110010100, "0110xx010x"));
+        assert!(bit_match(0b10110010101, "0110xx010x"));
+
+        assert!(bit_match(0b10110100100, "0110xx010x"));
+        assert!(bit_match(0b10110100101, "0110xx010x"));
+        assert!(bit_match(0b10110110100, "0110xx010x"));
+        assert!(bit_match(0b10110110101, "0110xx010x"));
+
+        assert!(!bit_match(0b10010100100, "0110xx010x"));
+        assert!(!bit_match(0b10110100111, "0110xx010x"));
+        assert!(!bit_match(0b10111110100, "0110xx010x"));
+        assert!(!bit_match(0b11110110101, "0110xx010x"));
+    }
+}
