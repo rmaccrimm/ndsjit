115a70fa1ed3b9ce694e59583d42380252d0ff11
Roddy MacCrimmon <r.maccrimm@gmail.com>
2023-01-29 10:08:08 -0700

    removing poc main code

--- src/main.rs
+++ src/main.rs
@@ -1,143 +1,42 @@
-use cranelift::prelude::InstBuilder;
-use std::mem;
+use std::{
+    error::Error,
+    io::{BufRead, BufReader, Write},
+    process::{ChildStdin, Command, Stdio},
+};
 
-use cranelift::codegen::ir::immediates::Offset32;
-use cranelift::codegen::ir::types::I32;
-use cranelift::codegen::ir::{AbiParam, Endianness, MemFlags, Signature};
-use cranelift::codegen::isa::CallConv;
-use cranelift::codegen::verifier::verify_function;
-use cranelift::codegen::{settings, Context};
-use cranelift::frontend::{FunctionBuilder, FunctionBuilderContext};
-use cranelift_jit::{JITBuilder, JITModule};
-use cranelift_module::{self, Module};
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut proc = Command::new("docker")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .args([
+            "run",
+            "--rm",
+            "-i",
+            "--log-driver=none",
+            "-a",
+            "stdin",
+            "-a",
+            "stdout",
+            "-a",
+            "stderr",
+            "asm",
+        ])
+        .spawn()?;
 
-#[derive(Copy, Clone, Debug)]
-#[repr(C)]
-struct VMState {
-    pub regs: [u32; 4],
-}
+    let input = "LDR r0,[r1,r2,LSL#30]!\nANDr1,r2,r3\n";
 
-struct CompiledCode {
-    ptr: *const u8,
-}
+    let child_in = proc.stdin.as_mut().unwrap();
+    child_in.write_all(&input.as_bytes()).unwrap();
+    proc.wait().expect("process failed");
 
-impl CompiledCode {
-    fn from_ptr(ptr: *const u8) -> Self {
-        Self { ptr }
+    let child_out = BufReader::new(proc.stdout.as_mut().unwrap());
+    // let mut child_err = BufReader::new(proc.stderr.as_mut().unwrap());
+
+    // let mut line_out = String::new();
+
+    for line in child_out.lines() {
+        println!("{}", line.unwrap());
     }
-
-    unsafe fn call(&self, vm_state: &mut VMState) {
-        let func: unsafe extern "C" fn(*mut u32) = mem::transmute(self.ptr);
-        func(vm_state.regs.as_mut_ptr());
-    }
-}
-
-pub struct Instruction {}
-
-struct InstrTranslator {
-    pub module: JITModule,
-    pub ctx: Context,
-    pub builder_ctx: FunctionBuilderContext,
-}
-
-impl Default for InstrTranslator {
-    fn default() -> Self {
-        let jit_builder = JITBuilder::new(cranelift_module::default_libcall_names()).unwrap();
-        let module = JITModule::new(jit_builder);
-        let ctx = module.make_context();
-
-        Self {
-            module,
-            ctx,
-            builder_ctx: FunctionBuilderContext::new(),
-        }
-    }
-}
-
-impl InstrTranslator {
-    fn translate_segment(&mut self, decoder: &mut BinaryDecoder) {
-        let int_type = self.module.target_config().pointer_type();
-        self.ctx.func.signature.params.push(AbiParam::new(int_type));
-        let mut builder = FunctionBuilder::new(&mut self.ctx.func, &mut self.builder_ctx);
-    }
-
-    fn translate_instr(instr: Instruction, builder: &mut FunctionBuilder) {
-        todo!()
-    }
-}
-
-struct BinaryDecoder<'a> {
-    data: &'a [u8],
-    pos: usize,
-    mode: bool,
-}
-
-impl BinaryDecoder<'_> {
-    fn next_instr(&mut self) {
-        todo!();
-    }
-}
-
-fn main() -> Result<(), String> {
-    // Goal - state transfer into/out of Function
-    let mut vm_state = VMState { regs: [0; 4] };
-    dbg!(vm_state);
-
-    let jit_builder =
-        JITBuilder::new(cranelift_module::default_libcall_names()).map_err(|e| e.to_string())?;
-    let mut module = JITModule::new(jit_builder);
-
-    let mut sig = Signature::new(CallConv::Fast);
-    // sig.params.push(AbiParam::new(I64));
-
-    let mut ctx = module.make_context();
-
-    let int_type = module.target_config().pointer_type();
-    ctx.func.signature.params.push(AbiParam::new(int_type));
-
-    let mut builder_ctx = FunctionBuilderContext::new();
-    let mut builder = FunctionBuilder::new(&mut ctx.func, &mut builder_ctx);
-
-    let block = builder.create_block();
-    builder.append_block_params_for_function_params(block);
-
-    builder.switch_to_block(block);
-    builder.seal_block(block);
-
-    let base = builder.block_params(block)[0];
-
-    let offset = Offset32::new(0);
-    let flags = MemFlags::trusted().with_endianness(Endianness::Little);
-    let reg0 = builder.ins().load(I32, flags, base, offset);
-
-    let imm = builder.ins().iconst(I32, 17);
-    let added = builder.ins().iadd(reg0, imm);
-    builder.ins().store(flags, added, base, offset);
-    builder.ins().return_(&[]);
-
-    builder.finalize();
-
-    let flags = settings::Flags::new(settings::builder());
-    verify_function(&ctx.func, &flags).map_err(|e| e.to_string())?;
-    println!("{}", ctx.func.display());
-
-    let func_id = module
-        .declare_anonymous_function(&mut sig)
-        .map_err(|e| e.to_string())?;
-    module
-        .define_function(func_id, &mut ctx)
-        .map_err(|e| e.to_string())?;
-
-    module.clear_context(&mut ctx);
-    module.finalize_definitions().unwrap();
-
-    let func_ptr = module.get_finalized_function(func_id);
-
-    unsafe {
-        let func: unsafe extern "C" fn(*mut u32) = mem::transmute(func_ptr);
-        func(vm_state.regs.as_mut_ptr());
-    }
-    dbg!(vm_state);
-
     Ok(())
 }
