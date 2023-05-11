292da61cf1f69d66cc5f5d7c2ba68c350b87a484
Roddy MacCrimmon <r.maccrimm@gmail.com>
2023-05-11 13:50:45 -0600

    create simple example program with conditional execution

--- src/asm/block_translator.rs
+++ src/asm/block_translator.rs
@@ -1,33 +1,39 @@
-use std::f32::consts::E;
-
-use cranelift::prelude::{AbiParam, EntityRef, GlobalValueData, InstBuilder, MemFlags};
+use cranelift::prelude::{AbiParam, EntityRef, GlobalValueData, InstBuilder, IntCC, MemFlags};
 use cranelift_codegen::ir::{
-    immediates::Offset32, types::I32, ArgumentPurpose, Function, GlobalValue,
+    immediates::Offset32,
+    types::{I32, I64},
+    ArgumentPurpose, Function, GlobalValue,
 };
 use cranelift_codegen::{settings, verify_function};
 use cranelift_frontend::{FunctionBuilder, FunctionBuilderContext, Variable};
 use cranelift_jit::{JITBuilder, JITModule};
 use cranelift_module::Module;
+use std::error::Error;
+use std::process::exit;
 
 use super::instruction_translator::translate_instruction;
 use crate::disasm::armv4t::{Instruction, Register};
 
+struct TranslationState {
+    register_vars: Vec<Variable>,
+}
+
 pub struct BlockTranslator {
     builder_ctx: FunctionBuilderContext,
-    register_vars: Vec<Variable>,
+    state: TranslationState,
 }
 
 impl BlockTranslator {
     pub fn new() -> Self {
         Self {
             builder_ctx: FunctionBuilderContext::new(),
-            register_vars: vec![],
+            state: TranslationState { register_vars: vec![] },
         }
     }
 
-    pub fn translate(&mut self, code: &Vec<Instruction>) {
+    pub fn translate(&mut self, code: &Vec<Instruction>) -> Result<*const u8, Box<dyn Error>> {
         let jit_builder = JITBuilder::new(cranelift_module::default_libcall_names()).unwrap();
-        let module = JITModule::new(jit_builder);
+        let mut module = JITModule::new(jit_builder);
         let mut ctx = module.make_context();
         let ptr_type = module.target_config().pointer_type();
 
@@ -39,64 +45,105 @@ impl BlockTranslator {
 
         let mut builder = FunctionBuilder::new(&mut ctx.func, &mut self.builder_ctx);
 
-        {
-            let entry_block = builder.create_block();
-            builder.append_block_params_for_function_params(entry_block);
-            builder.switch_to_block(entry_block);
-            builder.seal_block(entry_block);
+        let entry_block = builder.create_block();
+        builder.append_block_params_for_function_params(entry_block);
+        builder.switch_to_block(entry_block);
 
-            let vmctx = builder.create_global_value(GlobalValueData::VMContext);
-            // TODO some kind of trait that governs access to CPU state?
-            let registers = [
-                Register::R0,
-                Register::R1,
-                Register::R2,
-                Register::R3,
-                Register::R4,
-                Register::R5,
-                Register::R6,
-                Register::R7,
-                Register::R8,
-                Register::R9,
-                Register::R10,
-                Register::R11,
-                Register::R12,
-                Register::SP,
-                Register::LR,
-                Register::PC,
-                Register::FLAGS,
-            ];
+        let vmctx = builder.create_global_value(GlobalValueData::VMContext);
+        gen_prologue(vmctx, &mut self.state, &mut builder);
 
-            // Create a re-usable variable for each of the CPU registers
-            for (i, reg) in registers.iter().enumerate() {
-                let var = Variable::new(i);
-                builder.declare_var(var, I32);
-                self.register_vars.push(var);
+        // let mut current_block = entry_block;
 
-                let base = builder.ins().global_value(ptr_type, vmctx);
-                let tmp = builder.ins().load(I32, MemFlags::new(), base, 0);
-                builder.def_var(var, tmp);
-            }
+        // Start loop
 
-            let r2 = builder.use_var(self.register_vars[2]);
-            let const_ = builder.ins().iconst(I32, 10);
-            let tmp = builder.ins().iadd(r2, const_);
-            builder.def_var(self.register_vars[2], tmp);
+        // Code for conditional execution
+        let instr_block = builder.create_block();
+        let next_block = builder.create_block();
 
-            for (i, &var) in self.register_vars.iter().enumerate() {
-                let base = builder.ins().global_value(ptr_type, vmctx);
-                let arg = builder.use_var(var);
-                builder
-                    .ins()
-                    .store(MemFlags::new(), arg, base, 4 * (i as i32));
-            }
-            let const_ = builder.ins().iconst(I32, 0);
-            builder.ins().return_(&[const_]);
-            builder.finalize();
-        }
+        // Still filling prev (entry) block at this point
+        let flags = builder.use_var(self.state.register_vars[16]);
+        let zero = builder.ins().iconst(I32, 0);
+        let res = builder.ins().icmp_imm(IntCC::Equal, flags, 0);
+        builder.ins().brif(res, instr_block, &[], next_block, &[]);
+
+        builder.seal_block(instr_block);
+
+        builder.switch_to_block(instr_block);
+
+        // Actual instruction cond
+        let r2 = builder.use_var(self.state.register_vars[2]);
+        let const_ = builder.ins().iconst(I32, 99);
+        let tmp = builder.ins().iadd(r2, const_);
+        builder.def_var(self.state.register_vars[2], tmp);
+        builder.ins().jump(next_block, &[]);
+
+        builder.seal_block(next_block);
+        builder.switch_to_block(next_block);
+
+        // End loop
+
+        gen_epilogue(vmctx, &self.state, &mut builder);
+        builder.seal_all_blocks();
+        builder.finalize();
 
         let flags = settings::Flags::new(settings::builder());
-        verify_function(&ctx.func, &flags).unwrap();
+        verify_function(&ctx.func, &flags)?;
         println!("{}", ctx.func.display());
+
+        let func_id = module.declare_anonymous_function(&ctx.func.signature)?;
+        module.define_function(func_id, &mut ctx)?;
+
+        module.clear_context(&mut ctx);
+        module.finalize_definitions()?;
+
+        Ok(module.get_finalized_function(func_id))
     }
+}
+
+fn gen_prologue(vmctx: GlobalValue, state: &mut TranslationState, builder: &mut FunctionBuilder) {
+    // TODO some kind of trait that governs access to CPU state
+    let registers = [
+        Register::R0,
+        Register::R1,
+        Register::R2,
+        Register::R3,
+        Register::R4,
+        Register::R5,
+        Register::R6,
+        Register::R7,
+        Register::R8,
+        Register::R9,
+        Register::R10,
+        Register::R11,
+        Register::R12,
+        Register::SP,
+        Register::LR,
+        Register::PC,
+        Register::FLAGS,
+    ];
+
+    // Create a re-usable variable for each of the CPU registers
+    // TODO - some sort of context/environment managing this ptr type and other things like it
+    let base = builder.ins().global_value(I64, vmctx);
+    for (i, reg) in registers.iter().enumerate() {
+        let var = Variable::new(i);
+        builder.declare_var(var, I32);
+        state.register_vars.push(var);
+        let tmp = builder
+            .ins()
+            .load(I32, MemFlags::new(), base, 4 * (i as i32));
+        builder.def_var(var, tmp);
+    }
+}
+
+fn gen_epilogue(vmctx: GlobalValue, state: &TranslationState, builder: &mut FunctionBuilder) {
+    let base = builder.ins().global_value(I64, vmctx);
+    for (i, &var) in state.register_vars.iter().enumerate() {
+        let arg = builder.use_var(var);
+        builder
+            .ins()
+            .store(MemFlags::new(), arg, base, 4 * (i as i32));
+    }
+    let const_ = builder.ins().iconst(I32, 0);
+    builder.ins().return_(&[const_]);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,5 @@
 use std::{error::Error, mem, ptr};
 
-use cranelift::prelude::EntityRef;
-use cranelift::prelude::MemFlags;
-use cranelift_codegen::ir::immediates::Offset32;
-use cranelift_codegen::ir::types::*;
-use cranelift_codegen::ir::ArgumentPurpose;
-use cranelift_codegen::ir::{AbiParam, GlobalValueData, InstBuilder, Value};
-use cranelift_codegen::settings;
-use cranelift_codegen::verifier::verify_function;
-use cranelift_frontend::Variable;
-use cranelift_frontend::{FunctionBuilder, FunctionBuilderContext};
-use cranelift_jit::{JITBuilder, JITModule};
-use cranelift_module::{self, Module};
-
 use ndsjit::asm::block_translator::BlockTranslator;
 
 #[repr(C, packed)]
@@ -27,95 +14,15 @@ struct Regs {
 fn main() -> Result<(), Box<dyn Error>> {
     let mut translator = BlockTranslator::new();
     let code = vec![];
-    translator.translate(&code);
-    return Ok(());
+    let func_ptr = translator.translate(&code)?;
 
-    // OLD MAIN ------------------------------------------
-    let mut vm_state = Regs::default();
-    vm_state.r0 = 113;
-    vm_state.r1 = 999;
+    // let mut vm_state = Regs::default();
+    let mut vm_state = [0u32; 17];
+    vm_state[16] = 1;
     dbg!(vm_state);
 
-    let jit_builder =
-        JITBuilder::new(cranelift_module::default_libcall_names()).map_err(|e| e.to_string())?;
-    let mut module = JITModule::new(jit_builder);
-
-    let mut ctx = module.make_context();
-
-    let ptr_type = module.target_config().pointer_type();
-    dbg!(ptr_type);
-    ctx.func
-        .signature
-        .params
-        .push(AbiParam::special(ptr_type, ArgumentPurpose::VMContext));
-    ctx.func.signature.returns.push(AbiParam::new(I32));
-
-    let mut builder_ctx = FunctionBuilderContext::new();
-    {
-        let mut builder = FunctionBuilder::new(&mut ctx.func, &mut builder_ctx);
-        let vmctx = builder.create_global_value(GlobalValueData::VMContext);
-        let r0 = Variable::new(0);
-        builder.declare_var(r0, I32);
-
-        // let r0 = builder.create_global_value(GlobalValueData::Load {
-        //     base: vmctx,
-        //     offset: Offset32::new(0),
-        //     global_type: I32,
-        //     readonly: false,
-        // });
-
-        let block0 = builder.create_block();
-        builder.append_block_params_for_function_params(block0);
-
-        builder.switch_to_block(block0);
-        builder.seal_block(block0);
-        let base = builder.ins().global_value(ptr_type, vmctx);
-        {
-            let tmp = builder.ins().load(I32, MemFlags::new(), base, 0);
-            builder.def_var(r0, tmp);
-        }
-        {
-            let arg = builder.use_var(r0);
-            let const_ = builder.ins().iconst(I32, 10);
-            let tmp = builder.ins().iadd(arg, const_);
-            builder.def_var(r0, tmp);
-        }
-        {
-            let arg = builder.use_var(r0);
-            builder.ins().store(MemFlags::new(), arg, base, 0);
-            // let tmp = builder.ins().uload32(MemFlags::new(), base, 0);
-            // builder.def_var(r0, tmp);
-            // let ret = builder.use_var(r0);
-            builder.ins().return_(&[arg]);
-        }
-        builder.finalize();
-    }
-
-    // let flags = settings::Flags::new(settings::builder());
-    // let res = verify_function(&func, &flags);
-    // println!("{}", func.display());
-
-    let flags = settings::Flags::new(settings::builder());
-    verify_function(&ctx.func, &flags)?;
-    println!("{}", ctx.func.display());
-
-    // let mut sig = Signature::new(CallConv::SystemV);
-    // sig.params
-    //     .push(AbiParam::special(ptr_type, ArgumentPurpose::VMContext));
-    // sig.returns.push(AbiParam::new(I32));
-
-    let func_id = module.declare_anonymous_function(&ctx.func.signature)?;
-    // .map_err(|e| e.to_string())?;
-    module.define_function(func_id, &mut ctx)?;
-    // .map_err(|e| e.to_string())?;
-
-    module.clear_context(&mut ctx);
-    module.finalize_definitions().unwrap();
-
-    let func_ptr = module.get_finalized_function(func_id);
-
     unsafe {
-        let func: unsafe extern "C" fn(*mut Regs) -> i32 = mem::transmute(func_ptr);
+        let func: unsafe extern "C" fn(*mut [u32; 17]) -> i32 = mem::transmute(func_ptr);
         dbg!(func(ptr::addr_of_mut!(vm_state)));
     }
     dbg!(vm_state);
